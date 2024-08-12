a040ab0639ecf28c8d0db1feb5caac520de9bc5e
Alfredo Pasquel <alfredo.pasquel@gmail.com>
2024-08-12 09:41:40 -0400

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+/*
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+*/
+
+const form = document.querySelector("form")
+
+const ul = document.querySelector("#list ul")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newList = document.createElement("li");
+  newList.textContent = form["new-task-description"].value;
+  ul.appendChild(newList);
+  form.reset();
+  const button = document.createElement("button");
+  button.textContent = "x";
+  newList.appendChild(button);
+  button.addEventListener("click", (e) => {
+    newList.remove();
+  });
+});
+
+
